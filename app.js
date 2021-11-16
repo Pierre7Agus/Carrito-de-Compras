095e5b229b5d81a9a51369db2f6b41d0b60ea7b2
Pierre7Agus <redtor_90@outlook.es>
2021-11-15 20:08:19 -0500

    Add paintCards function The paintCards function allows to present the products to the user, receives an array as a parameter, which is iterated over to modify the value of the elements

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+const templateCard = document.getElementById('template-card').content;
+const cards = document.getElementById('cards');
+const fragment = document.createDocumentFragment();
+
+
+
 const getInformation = async()=>{
     try{
         let data  = await fetch('./data/data.json')
         data = await data.json()
+        paintCards(data)
     }catch(error){
         console.log(error)
     }
@@ -11,3 +18,18 @@ document.addEventListener('DOMContentLoaded',()=>{
     getInformation()
 })
 
+
+const paintCards = (data) =>{
+    data.forEach(element => {
+        templateCard.querySelector('img').setAttribute('src',element.Url)
+        templateCard.querySelector('img').setAttribute('alt',element.title)
+        templateCard.querySelector('h5').textContent=element.title
+        templateCard.querySelector('p').textContent=element.precio
+        templateCard.querySelector('button').dataset.id=element.id
+
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    });
+
+    cards.appendChild(fragment)
+}
