93feed12d8a48e0203eba1b867d73a6483ceb85c
Pierre7Agus <redtor_90@outlook.es>
2021-11-15 19:26:08 -0500

    Add addEventListener Add an AddEventListener to execute the getInformation function and modify the getInformation function by adding a try and catch

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const getInformation = async()=>{
-    let data  = await fetch('./data/data.json')
-    data = await data.json()
-    console.log(data)
-
+    try{
+        let data  = await fetch('./data/data.json')
+        data = await data.json()
+        console.log(data)
+    }catch(error){
+        console.log(error)
+    }
 }
+
+document.addEventListener('DOMContentLoaded',()=>{
+    getInformation()
+})
+
