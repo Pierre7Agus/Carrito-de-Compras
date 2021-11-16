d6066b9e92202803e51e23a1b857cb7acbd00674
Pierre7Agus <redtor_90@outlook.es>
2021-11-15 19:42:59 -0500

    Add constants Add the constant templateCard, which will allow to obtain the content of the element with the id of template-card. Add the constant cards, which will allow obtaining the reference to the element with id cards, which will be the parent element of the fragment. Add the constant fragment, in which the creation of the documentFragment interface is stored.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const getInformation = async()=>{
     try{
         let data  = await fetch('./data/data.json')
         data = await data.json()
-        console.log(data)
     }catch(error){
         console.log(error)
     }
